3b5dcab2a7787bdbf5db6da25f0384b38e40b0b0
muerterauda <juanpalmaborda@uma.es>
2021-11-19 17:21:51 +0100

    Cuarto commit actualizando los setup y creando pyproject.toml

--- setup.py
+++ setup.py
@@ -1,38 +1,27 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='mongo_manager',
-    packages=['mongo_manager'],
-    version='0.1',
-    license='MIT',
-    description='Libreria para manejar objetos almacenados en MongoDB, usando la referencia de los CRUDRepository de SpringBoot',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mongo-manager-juan-palma-borda",
+    version="0.0.1",
     author='Juan Palma Borda',
     author_email='juanpalmaborda@hotmail.com',
-    url='https://github.com/muerterauda/mongo_manager',
-    download_url='https://github.com/muerterauda/mongo_manager/archive/v_0_1.tar.gz',
-    keywords=['mongo', 'mongodb', 'object', 'reposiroty', 'entity'],
-    install_requires=[
-        'bson',
-        'pymongo',
+    description='Libreria para manejar objetos almacenados en MongoDB, '
+                'usando la referencia de los CRUDRepository de SpringBoot',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/muerterauda/mongo_manager",
+    project_urls={
+        "Bug Tracker": "https://github.com/muerterauda/mongo_manager/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
