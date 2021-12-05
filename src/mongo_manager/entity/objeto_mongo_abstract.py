f5ca8fd0bd581f90bb10125817f5f1426b299952
muerterauda <juanpalmaborda@hotmail.es>
2021-12-05 12:32:36 +0100

    Version 0.2.1. Mejora y definicion de los ultimos metodos de ObjetoMongoAbstact, especificacion de los returns de la mayoria de los metodos de ReporsitoryBase.

--- src/mongo_manager/entity/objeto_mongo_abstract.py
+++ src/mongo_manager/entity/objeto_mongo_abstract.py
@@ -1,41 +1,68 @@
 import json
-from abc import ABC, abstractmethod
+from abc import ABC
+
+from bson import ObjectId
 
 
 class ObjetoMongoAbstract(ABC):
 
-    def __init__(self, id_mongo=None):
-        self.__id_mongo = id_mongo
+    def __init__(self, _id=None):
+        self._id = _id
 
     @property
-    def id_mongo(self):
-        return self.__id_mongo
+    def id(self) -> ObjectId:
+        return self._id
 
-    def get_dict(self):
-        d = self.get_dict_no_id()
-        d['_id'] = self.id_mongo
+    def get_dict(self, id_mongo=True, id_as_string=False) -> dict:
+        """
+        Return all variables from this object, similar to vars(object) with exception for _id
+        @param id_mongo: True -> Return dict with _id
+        @param id_as_string: True -> Return _id as str if id_mongo is True
+        @return: dict with object variables, similar to vars(object)
+        """
+        d = vars(self).copy()
+        if not id_mongo:
+            d.pop('_id')
+        elif id_as_string and self._id is not None:
+            d['_id'] = str(self._id)
         return d
 
-    @abstractmethod
-    def get_dict_no_id(self):
-        ...
+    def get_dict_no_id(self) -> dict:
+        """
+        Fachade for get_dict(id_mongo=False). Return all variables from this object, similar to vars(object) with exception for _id
+        @return: dict with object variables, similar to vars(object)
+        """
+        return self.get_dict(id_mongo=False)
 
-    def serialize(self):
-        return json.dumps(self.get_dict_no_id())
+    def serialize(self, id_mongo=True) -> str:
+        """
+        Serialize object to JSON format
+        @param id_mongo: True -> Append _id to json
+        @return: str in Json format
+        """
+        return json.dumps(self.get_dict(id_mongo=id_mongo, id_as_string=True))
 
     @staticmethod
-    def serialize_all(objetos):
-        return json.dumps(ObjetoMongoAbstract.generar_list_dicts_from_list_objects(objetos))
+    def serialize_all(objetos, id_mongo=True) -> str:
+        """
+        Serialize all objects to JSON format
+        @param objetos: List for object to convert to JSON
+        @param id_mongo: True -> Append _id to json
+        @return: str in Json format
+        """
+        return json.dumps(ObjetoMongoAbstract.generar_list_dicts_from_list_objects(objetos, id_mongo=id_mongo,
+                                                                                   id_as_string=True))
 
     @staticmethod
-    @abstractmethod
-    def generar_object_from_dict(dictionary):
-        pass
+    def generar_object_from_dict(dictionary, cls):
+        if dictionary is None:
+            return None
+        return cls(**dictionary)
 
     @staticmethod
     def generar_objects_from_list_dicts(dictionaries: list, cls):
-        return [cls.generar_object_from_dict(dictionary) for dictionary in dictionaries]
+        return [cls.generar_object_from_dict(dictionary, cls) for dictionary in dictionaries]
 
     @staticmethod
-    def generar_list_dicts_from_list_objects(lista_objetos: list):
-        return [c.get_dict_no_id() for c in lista_objetos]
+    def generar_list_dicts_from_list_objects(lista_objetos: list, id_mongo=True, id_as_string=False):
+        return [c.get_dict(id_mongo=id_mongo, id_as_string=id_as_string) for c in lista_objetos]
--- src/mongo_manager/repository/repository_base.py
+++ src/mongo_manager/repository/repository_base.py
@@ -1,5 +1,9 @@
-import pymongo
+from typing import Any
 
+import pymongo
+from pymongo.results import UpdateResult, InsertManyResult, InsertOneResult, DeleteResult
+
+from ..entity.objeto_mongo_abstract import ObjetoMongoAbstract
 from ..mongo_manager import SingletonMeta
 from bson import ObjectId
 
@@ -20,36 +24,36 @@ class RepositoryBase(metaclass=SingletonMeta):
     def clase(self):
         return self.__clase
 
-    def count_all(self):
+    def count_all(self) -> Any:
         return self.collection.count_documents({})
 
-    def get_all(self, skip=0, limit=1000):
+    def get_all(self, skip=0, limit=1000) -> list:
         return self.clase.generar_objects_from_list_dicts(self.collection.find().skip(skip).limit(limit), self.clase)
 
-    def find_by_id(self, id_mongo):
-        return self.clase.generar_object_from_dict(self.collection.find_one({'_id': ObjectId(id_mongo)}))
+    def find_by_id(self, id_mongo) -> ObjetoMongoAbstract:
+        return self.clase.generar_object_from_dict(self.collection.find_one({'_id': ObjectId(id_mongo)}), self.clase)
 
-    def delete_object(self, objeto):
-        if objeto.id_mongo is not None:
-            return self.delete_by_id(objeto.id_mongo)
+    def delete_object(self, objeto) -> DeleteResult:
+        if objeto.id is not None:
+            return self.delete_by_id(objeto.id)
 
-    def delete_by_id(self, id_mongo):
+    def delete_by_id(self, id_mongo) -> DeleteResult:
         return self.collection.delete_one({'_id': ObjectId(id_mongo)})
 
-    def insert_one(self, objeto):
+    def insert_one(self, objeto) -> InsertOneResult:
         return self.collection.insert_one(objeto.get_dict_no_id())
 
-    def insert_many(self, lista_objetos: list):
+    def insert_many(self, lista_objetos: list) -> InsertManyResult:
         return self.collection.insert_many(self.clase.generar_list_dicts_from_list_objects(lista_objetos))
 
     def insert_or_replace_id(self, objeto):
-        if objeto.id_mongo is None:
+        if objeto.id is None:
             return self.insert_one(objeto)
         else:
-            return self.replace_by_id(objeto.id_mongo, objeto)
+            return self.replace_by_id(objeto.id, objeto)
 
-    def replace_by_id(self, id_mongo, objeto):
+    def replace_by_id(self, id_mongo, objeto) -> UpdateResult:
         return self.collection.replace_one({"_id": id_mongo}, objeto.get_dict())
 
-    def update_by_id(self, id_mongo, objeto_dict: dict):
+    def update_by_id(self, id_mongo, objeto_dict: dict) -> UpdateResult:
         return self.collection.update_one({"_id": id_mongo}, {"$set": objeto_dict})
