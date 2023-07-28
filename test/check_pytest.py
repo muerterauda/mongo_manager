c33fb9f96ecfd6823e3e2380d74ec8d351b4a107
muerterauda <juanpalmaborda@hotmail.es>
2023-07-28 12:51:01 +0200

    Probando los test de pytest

--- test/check_pytest.py
+++ test/check_pytest.py
@@ -1,5 +1,14 @@
 import pytest
 
 
-def test_pytest():
-    assert 1 == 1
+@pytest.fixture
+def number():
+    return 1
+
+
+def test_pytest(number):
+    assert 1 == number
+
+
+def test_pytest_2(number):
+    assert 2 == number
